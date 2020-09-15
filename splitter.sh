e0267c231dd848fd91b67a5afb3601d31b434b44
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-09-15 14:00:35 +0200

    Remove ghostscript logs

--- splitter.sh
+++ splitter.sh
@@ -11,8 +11,8 @@ fi
 
 # render pdf with to get a doubled width pdf, slides | notes
 # \setbeameroption{show notes on second screen}
-gs -o $slideshow_name -sDEVICE=pdfwrite -c "[/CropBox [0 0 362.835 272.126]" -c " /PAGES pdfmark" -f $file
-gs -o $notes_name -sDEVICE=pdfwrite -c "[/CropBox [362.835 0 725.669 272.126]" -c " /PAGES pdfmark" -f $file
+gs -o $slideshow_name -sDEVICE=pdfwrite -c "[/CropBox [0 0 362.835 272.126]" -c " /PAGES pdfmark" -f $file 1>/dev/null
+gs -o $notes_name -sDEVICE=pdfwrite -c "[/CropBox [362.835 0 725.669 272.126]" -c " /PAGES pdfmark" -f $file 1>/dev/null
 
 echo "- your slides are in $slideshow_name"
 echo "- your notes are in $notes_name"
